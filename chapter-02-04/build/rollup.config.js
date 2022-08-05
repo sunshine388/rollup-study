47d8ac60164814231dbbbc2e335e1bb9a0d9b656
熄月 <2723166086@qq.com>
2022-08-05 18:08:19 +0800

    feat：chapter-02-04

---
+++ chapter-02-04/build/rollup.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const { babel } = require("@rollup/plugin-babel");
+const nodeResolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+
+const resolveFile = function (filePath) {
+  return path.join(__dirname, "..", filePath);
+};
+
+const babelOptions = {
+  presets: ["@babel/preset-env"],
+};
+
+module.exports = [
+  {
+    input: resolveFile("src/index.js"),
+    output: {
+      file: resolveFile("dist/index.js"),
+      format: "umd",
+    },
+    plugins: [nodeResolve(), commonjs(), babel(babelOptions)],
+  },
+];
