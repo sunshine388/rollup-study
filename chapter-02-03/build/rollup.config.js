59abac7101535e91af3c8e1cf58d9e307741bc05
熄月 <2723166086@qq.com>
2022-08-05 18:00:28 +0800

    feat：chapter-02-03

---
+++ chapter-02-03/build/rollup.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const { babel } = require("@rollup/plugin-babel");
+
+const resolveFile = function (filePath) {
+  return path.join(__dirname, "..", filePath);
+};
+
+const babelOptions = {
+  presets: ["@babel/preset-env"],
+};
+
+const plugins = [babel(babelOptions)];
+
+module.exports = [
+  {
+    input: resolveFile("src/index.js"),
+    output: {
+      file: resolveFile("dist/index.js"),
+      format: "umd",
+      name: "Demo",
+    },
+    external: ["lib/hello", "lib/world"],
+    plugins,
+  },
+
+  {
+    input: resolveFile("src/lib/hello.js"),
+    output: {
+      file: resolveFile("dist/lib/hello.js"),
+      format: "umd",
+      name: "Hello",
+    },
+    plugins,
+  },
+
+  {
+    input: resolveFile("src/lib/world.js"),
+    output: {
+      file: resolveFile("dist/lib/world.js"),
+      format: "umd",
+      name: "World",
+    },
+    plugins,
+  },
+];
