7c15e110b2f180487ae95b85ac2ddedd73fe93c6
熄月 <2723166086@qq.com>
2022-08-05 18:14:39 +0800

    feat：chapter-03-01

---
+++ chapter-03-01/build/rollup.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const { babel } = require("@rollup/plugin-babel");
+const postcss = require("rollup-plugin-postcss");
+
+const resolveFile = function (filePath) {
+  return path.join(__dirname, "..", filePath);
+};
+
+const isProductionEnv = process.env.NODE_ENV === "production";
+
+const babelOptions = {
+  presets: ["@babel/preset-env"],
+};
+
+module.exports = [
+  {
+    input: resolveFile("src/index.js"),
+    output: {
+      file: resolveFile("dist/index.js"),
+      format: "umd",
+    },
+    plugins: [
+      postcss({
+        extract: true,
+        minimize: isProductionEnv,
+      }),
+      babel(babelOptions),
+    ],
+  },
+];
