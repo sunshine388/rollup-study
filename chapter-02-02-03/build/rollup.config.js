c3bbb57cf905d1852abc077d7700e6cb36277b37
熄月 <2723166086@qq.com>
2022-08-05 17:34:58 +0800

    feat：chapter-02-02-03

---
+++ chapter-02-02-03/build/rollup.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const { babel } = require("@rollup/plugin-babel");
+
+const resolveFile = function (filePath) {
+  return path.join(__dirname, "..", filePath);
+};
+
+const babelOptions = {
+  presets: ["@babel/preset-env"],
+};
+
+module.exports = [
+  {
+    input: resolveFile("src/index.js"),
+    output: {
+      file: resolveFile("dist/index.js"),
+      format: "cjs",
+    },
+
+    plugins: [babel(babelOptions)],
+  },
+];
