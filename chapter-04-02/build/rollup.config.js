9f9905f0d106821086fc6f1fb83b6f95a30e724e
熄月 <2723166086@qq.com>
2022-08-05 23:04:04 +0800

    feat：chapter-04-02

---
+++ chapter-04-02/build/rollup.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const buble = require("@rollup/plugin-buble");
+const { babel } = require("@rollup/plugin-babel");
+const nodeResolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const vue = require("rollup-plugin-vue");
+const css = require("rollup-plugin-css-only");
+const replace = require("@rollup/plugin-replace");
+
+const resolveFile = function (filePath) {
+  return path.join(__dirname, "..", filePath);
+};
+
+// const isProductionEnv = process.env.NODE_ENV === 'production';
+
+const babelOptions = {
+  presets: ["@babel/preset-env"],
+};
+
+module.exports = [
+  {
+    input: resolveFile("src/index.js"),
+    // input: resolveFile('src/App.vue'),
+    output: {
+      file: resolveFile("dist/index.js"),
+      format: "iife",
+      name: "App",
+    },
+    // external: ['vue'],
+    plugins: [
+      vue({ css: false }),
+      css(),
+      nodeResolve(),
+      commonjs(),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      }),
+      babel(babelOptions),
+      buble(),
+    ],
+  },
+];
---
+++ chapter-04-02/src/index.js
@@ -0,0 +1,5 @@
+// import Vue from 'vue/dist/vue';
+import Vue from "vue/index";
+import App from "./App.vue";
+
+Vue.createApp(App).mount("#App");
